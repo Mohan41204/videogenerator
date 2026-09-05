8228944749dc8c24cc93fa84e5d1e53d1e2a5525
Muthuram-45 <muthuram921@gmail.com>
2026-09-05 18:06:56 +0530

    url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://videogenerator-backend-ws81.onrender.com';
-export const API_BASE_URL = 'http://localhost:5000';
+ export const API_BASE_URL = 'https://videogenerator-backend-ws81.onrender.com';
+//export const API_BASE_URL = 'http://localhost:5000';
 export const getFullUrl = (path) => {
   if (!path) return '';
   if (path.startsWith('http://') || path.startsWith('https://')) return path;
